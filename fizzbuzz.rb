9262be57984691e251e15465d1bb5f2a5a32e997
modern-helper-8600 <bennett.melissa1@gmail.com>
2020-03-18 05:35:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -15,6 +15,9 @@ end
 
 
 fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
+fizzbuzz(4)
 
   # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
