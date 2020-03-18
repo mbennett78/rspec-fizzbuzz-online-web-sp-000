4494bff8ab6eeacaa7a8d0b167df265881818d6c
modern-helper-8600 <bennett.melissa1@gmail.com>
2020-03-18 04:25:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
-# Don't forget! This file needs to be 'required' in its spec file
+def fizzbuzz(number)
+  if number.modulo(15)==0
+    puts "FizzBuzz"
+     elsif number.modulo(5)==0
+       puts "Buzz"
+        elsif number.modulo(3)==0
+          puts "Fizz"
+           else nil
+    end
+end
+
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
+fizzbuzz(4)
+
+  # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
